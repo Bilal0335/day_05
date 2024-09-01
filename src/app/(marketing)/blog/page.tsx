bdd44ca61fdbab43e607e9f05c112e56cf91515d
Bilal0335 <bilalhussain42201.com>
2024-09-01 23:56:56 +0500

    update

---
+++ src/app/(marketing)/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h3>Blog Page</h3>
+    </>
+  );
+}
---
+++ src/app/(shop)/account/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h3>Account Page</h3>
+    </>
+  );
+}
